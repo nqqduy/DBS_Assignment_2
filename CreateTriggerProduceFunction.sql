5ea5bb0f5de5290fba53b8fe99f355372dbb93ab
binhtran432k <binhtran432k@gmail.com>
2020-12-27 18:17:53 +0700

    Them vai trigger cho rang buoc va du lieu khoi dau

--- CreateTriggerProduceFunction.sql
+++ CreateTriggerProduceFunction.sql
@@ -1,14 +1,73 @@
+-- Rang buoc boi trigger
 DELIMITER $$
-DROP TRIGGER IF EXISTS CongTyVanChuyen.TrigKmTraDnThoai$$
-
-CREATE TRIGGER CongTyVanChuyen.TrigKmTraDnThoai BEFORE INSERT ON CongTyVanChuyen.data
+-- Trigger BienBanNhanHang
+DROP TRIGGER IF EXISTS CongTyVanChuyen.trigger_insert_BienBanNhanHang$$
+CREATE TRIGGER CongTyVanChuyen.trigger_insert_BienBanNhanHang BEFORE INSERT ON CongTyVanChuyen.BienBanNhanHang
 FOR EACH ROW 
-BEGIN 
-IF (NEW.phone REGEXP '^0[0-9]{9}$' ) = 0 THEN 
-    SIGNAL SQLSTATE '12345'
-        SET MESSAGE_TEXT = 'The phone number must have 10 digits and begin with 0!!!';
+BEGIN
+DECLARE ngayGui DATE;
+SELECT NgayGui FROM CongTyVanChuyen.BienBangHang
+WHERE IdBienBan = NEW.IdBbanN
+INTO ngayGui;
+IF (NEW.NgayNhan <= ngayGui) THEN 
+	SIGNAL SQLSTATE '10001'
+		SET MESSAGE_TEXT = 'Ngay nhan hang phai dien ra sau ngay gui hang!!!';
+ELSEIF (EXISTS(SELECT * FROM CongTyVanChuyen.BienBanGuiHang
+WHERE IdBbanG = NEW.IdBbanN)) THEN 
+	SIGNAL SQLSTATE '10002'
+		SET MESSAGE_TEXT = 'Id cua bien ban nay da ton tai o bien ban nhan gui!!!';
+END IF; 
+END$$
+DROP TRIGGER IF EXISTS CongTyVanChuyen.trigger_update_BienBanNhanHang$$
+CREATE TRIGGER CongTyVanChuyen.trigger_update_BienBanNhanHang BEFORE UPDATE ON CongTyVanChuyen.BienBanNhanHang
+FOR EACH ROW 
+BEGIN
+DECLARE ngayGui DATE;
+SELECT NgayGui FROM CongTyVanChuyen.BienBangHang
+WHERE IdBienBan = NEW.IdBbanN
+INTO ngayGui;
+IF (NEW.NgayNhan <= ngayGui) THEN 
+	SIGNAL SQLSTATE '10001'
+		SET MESSAGE_TEXT = 'Ngay nhan hang phai dien ra sau ngay gui hang!!!';
+END IF; 
+END$$
+-- Trigger BienBanGuiHang
+DROP TRIGGER IF EXISTS CongTyVanChuyen.trigger_insert_BienBanGuiHang$$
+CREATE TRIGGER CongTyVanChuyen.trigger_insert_BienBanGuiHang BEFORE INSERT ON CongTyVanChuyen.BienBanGuiHang
+FOR EACH ROW 
+BEGIN
+IF (EXISTS(SELECT * FROM CongTyVanChuyen.BienBanNhanHang
+WHERE IdBbanN = NEW.IdBbanG)) THEN 
+	SIGNAL SQLSTATE '10001'
+		SET MESSAGE_TEXT = 'Id cua bien ban nay da ton tai o bien ban nhan hang!!!';
+END IF; 
+END$$
+-- Trigger CuocXeLienTinh
+DROP TRIGGER IF EXISTS CongTyVanChuyen.trigger_insert_CuocXeLienTinh$$
+CREATE TRIGGER CongTyVanChuyen.trigger_insert_CuocXeLienTinh BEFORE INSERT ON CongTyVanChuyen.CuocXeLienTinh
+FOR EACH ROW 
+BEGIN
+DECLARE ngayDi DATE;
+SELECT NgayDi FROM CongTyVanChuyen.ThongTinCuocXe
+WHERE IdCuocXe = NEW.IdCuocXeLt
+INTO ngayDi;
+IF (NEW.NgayDen <= ngayDi) THEN 
+	SIGNAL SQLSTATE '10002'
+		SET MESSAGE_TEXT = 'Ngay den cua cuoc xe lien tinh phai dien ra sau ngay di!!!';
+END IF; 
+END$$
+
+DROP TRIGGER IF EXISTS CongTyVanChuyen.trigger_update_CuocXeLienTinh$$
+CREATE TRIGGER CongTyVanChuyen.trigger_update_CuocXeLienTinh BEFORE UPDATE ON CongTyVanChuyen.CuocXeLienTinh
+FOR EACH ROW 
+BEGIN
+DECLARE ngayDi DATE;
+SELECT NgayDi FROM CongTyVanChuyen.ThongTinCuocXe
+WHERE IdCuocXe = NEW.IdCuocXeLt
+INTO ngayDi;
+IF (NEW.NgayDen <= ngayDi) THEN 
+	SIGNAL SQLSTATE '10002'
+		SET MESSAGE_TEXT = 'Ngay den cua cuoc xe lien tinh phai dien ra sau ngay di!!!';
 END IF; 
 END$$
 DELIMITER ;
-
-INSERT INTO CongTyVanChuyen.data VALUES ('08524567889');
---
+++ InitialData.sql
@@ -0,0 +1,72 @@
+-- NhanVien
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206239210','Trần Đức Bình','12 Ngô Đức Kế, Quận 12','Hồ Chí Minh','binh@gmail.com','M','1999-03-14','15000','e7010e40c59fd8d4b0760d9bf1c43e35f09cc9fd016f07bc8384c2b605aade3f','HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206245953','Nguyễn Ngọc Chính','12 Ngô Đức Kế, Quận 12','Hồ Chí Minh','chinh@gmail.com','M','1999-03-14','15000','a4ed29678c06986d3b2668da4e684767064fd14385c2c51fd402a3c9dade9c98','HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206232876', 'Nguyễn Văn Chung', '05 Trần Quốc Toản, Huyện Quảng Xương', 'Thanh Hóa', 'chung@gmail.com', 'M', '2000-05-19', '12000', 'ed1f19ae96267ae162e0bc92c7f604778444cbda7f694d7e8ac31418bea86ae7', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206245678', 'Trần Đình Dũng', '25 Nguyễn Ánh, Huyện Cẩm Xuyên', 'Hà Tĩnh', 'dung@gmail.com', 'M', '1998-02-19', '10000', '2bf33dad05eb008b7610b9c89b4f3dabe96e4a58635e623382ba748891e4c71a', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206456789', 'Nguyễn Quốc Duy', '30 Lê Minh Đảo, Huyện Phú Xuân Hạ', 'Quảng Tín', 'duy@gmail.com', 'M', '2001-04-23', '13000', 'e58ff872611207784913736b428ab7423910f28ef1bbfcb353517a4afb6fa8fa', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206341234', 'Phan Thị Ánh Dương', '02 Đồng Khởi, Quận 1', 'Hồ Chí Minh', 'duong@gmail.com', 'F', '2000-12-12', '14000', '033072dc94854191ac69e2353b3ddca4c186345979a132688868a14fb3298df6', 'DX');	
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206289456', 'Nguyễn Văn Không Quen', '10 Trần Lãm, Quận 3', 'Hồ Chí Minh', NULL, 'M', '1990-10-10', '9000', '58e6a8e95ce1b94b9393f7533d17016b2f73f5f3978729ca19b5b86b5869af90', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206258124', 'Trần A', '20 Nguyễn Thái Học, Quận 11', 'Hồ Chí Minh', 'a@gmail.com', 'M', '1980-12-17', '9000', '7c04837eb356565e28bb14e5a1dedb240a5ac2561f8ed318c54a279fb6a9665e', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206491789', 'Trần B', '22 Nguyễn Văn Cừ, Quận 5', 'Hồ Chí Minh', NULL, 'M', '1970-05-16', '8000', '60bcad77c43c4ab9840d8aa44b6c18e692e6c8e5e4f40351561c41274f1197c8', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206239233', 'Nguyễn Thị Xuân', '16 Nguyễn Trãi, Quận Thủ Đức', 'Hồ Chí Minh', 'xuan@gmail.com', 'F', '1978-12-03', '10000', 'e115d520ab04829823d68d50a1a679caf92ab2fa87f349bea6b261288cc4dc46', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206247294', 'Nguyễn Nhân', '12 Dân Chủ, Huyện Gò Công', 'Tiền Giang', 'nhan@gmail.com', 'M', '1992-11-23', '9000', '38e49e552db2bb25844b55f39805d5f818ecf929988c386d034bee85176551eb', 'HD');
+INSERT INTO CongTyVanChuyen.NhanVien(Cccd, HoTen, DiaChiNha, Tinh, Email, GioiTinh, NgaySinh, Luong, MatKhau, TrangThaiNv)
+VALUES ('012206210267', 'Nguyễn Xuân Quân', '03 Ba Đình, Quận Ba Đình', 'Hà Nội', 'quan@gmail.com', 'M', '1990-04-04', '8000', '658d761f5da300da6e2ab74b7571f0045519e06eb6917d2817647aacc31341ef', 'DX');
+
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (1,'0903456123');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (2, '0898125108');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (3, '0983285736');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (4, '0909346178');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (5, '0908765345');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (6, '0344278456');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (7, '0905076458');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (8, '0905125786');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (9, '0344294123');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (10, '0534128194');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (11, '0782345678');
+INSERT INTO CongTyVanChuyen.DienThoaiNv VALUES (12, '0912345789');
+
+INSERT INTO CongTyVanChuyen.NhanVienVanChuyen VALUES (7, 'C', '2019-10-10');
+INSERT INTO CongTyVanChuyen.NhanVienVanChuyen VALUES (8, 'C', '2018-03-02');
+INSERT INTO CongTyVanChuyen.NhanVienVanChuyen VALUES (9, 'C', '2020-01-02');
+INSERT INTO CongTyVanChuyen.NhanVienVanChuyen VALUES (10, 'D', '2017-01-02');
+INSERT INTO CongTyVanChuyen.NhanVienVanChuyen VALUES (11, 'E', '2018-09-08');
+
+INSERT INTO CongTyVanChuyen.NhanVienKho(IdNvKho, LoaiNv)
+VALUES (1, 'QK');
+INSERT INTO CongTyVanChuyen.NhanVienKho(IdNvKho, LoaiNv)
+VALUES (2, 'QK');
+INSERT INTO CongTyVanChuyen.NhanVienKho(IdNvKho, LoaiNv)
+VALUES (3, 'QK');
+INSERT INTO CongTyVanChuyen.NhanVienKho(IdNvKho, LoaiNv)
+VALUES (4, 'QK');
+INSERT INTO CongTyVanChuyen.NhanVienKho(IdNvKho, LoaiNv)
+VALUES (5, 'QK');
+
+INSERT INTO CongTyVanChuyen.Kho(TenKho, Tinh, DienTich, IdQuanLy, TrangThaiKho, SucChua, SucChuaHt)
+VALUES ('Linh Xuân', 'Hồ Chí Minh', '10', '1', 'HD', '20', '7');
+INSERT INTO CongTyVanChuyen.Kho(TenKho, Tinh, DienTich, IdQuanLy, TrangThaiKho, SucChua, SucChuaHt)
+VALUES ('Khu Chế Xuất', 'Đồng Nai', '4', '2', 'HD', '10', '9');
+INSERT INTO CongTyVanChuyen.Kho(TenKho, Tinh, DienTich, IdQuanLy, TrangThaiKho, SucChua, SucChuaHt)
+VALUES ('Tân An', 'Long An', '7', '3', 'HD', '15', '13');
+INSERT INTO CongTyVanChuyen.Kho(TenKho, Tinh, DienTich, IdQuanLy, TrangThaiKho, SucChua, SucChuaHt)
+VALUES ('Thắng Tam', 'Vũng Tàu', '6', '4', 'HD', '15', '4');
+INSERT INTO CongTyVanChuyen.Kho(TenKho, Tinh, DienTich, IdQuanLy, TrangThaiKho, SucChua, SucChuaHt)
+VALUES ('Chợ Gạo', 'Tiền Giang', '12', '5', 'HD', '20', '15');
+
+UPDATE CongTyVanChuyen.NhanVienKho SET IdKhoLam = 1 WHERE (`IdNvKho` = 1);
+UPDATE CongTyVanChuyen.NhanVienKho SET IdKhoLam = 2 WHERE (`IdNvKho` = 2);
+UPDATE CongTyVanChuyen.NhanVienKho SET IdKhoLam = 3 WHERE (`IdNvKho` = 3);
+UPDATE CongTyVanChuyen.NhanVienKho SET IdKhoLam = 4 WHERE (`IdNvKho` = 4);
+UPDATE CongTyVanChuyen.NhanVienKho SET IdKhoLam = 5 WHERE (`IdNvKho` = 5);
